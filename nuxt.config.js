fc8a2370fb6927cb118c3e4d547006d09c6b5d10
Deepjyoti Barman <deep.barman30@gmail.com>
2021-03-02 13:23:08 +0530

    Add basic code to handle routing

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    "~/assets/scss/app.scss"
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -44,5 +45,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  // Change port
+  server: {
+    port: 8080
   }
 }
---
+++ pages/_post.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Mountain: {{ post }}</h2>
+    <p>Path: {{ $route.path }}</p>
+    <NuxtLink to="/">Back to Mountains</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: this.$route.params.parent,
+    }
+  },
+  async asyncData({ params, redirect }) {
+    return {
+        post: params.parent
+    }
+  }
+}
+</script>
