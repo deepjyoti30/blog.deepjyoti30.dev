c6331e363031b73a392cebd79fc5b3669e263c1d
Deepjyoti Barman <deep.barman30@gmail.com>
2021-03-04 22:18:48 +0530

    Add some icons for the social content in the footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,7 +5,46 @@
             <div class="copyright--content">
                 © 2020-Present Deepjyoti Barman. All rights reserved.
             </div>
-            <div class="social--content"></div>
+            <div class="social--content inline-flex mr-auto ml-auto mt-4">
+                <span>
+                    <github-icon class="icon" />
+                </span>
+                <span>
+                    <linkedin-icon class="icon" />
+                </span>
+                <span>
+                    <twitter-icon class="icon" />
+                </span>
+                <span>
+                    <mail-icon class="icon" />
+                </span>
+            </div>
         </div>
     </div>
 </template>
+
+<script>
+import { GithubIcon, TwitterIcon, LinkedinIcon, MailIcon } from "vue-feather-icons";
+
+export default {
+    components: {
+        GithubIcon,
+        TwitterIcon,
+        LinkedinIcon,
+        MailIcon
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.social--content {
+    span {
+        @apply mx-2;
+
+        .icon {
+            width: 1.3em;
+            height: 1.3em;
+        }
+    }
+}
+</style>
