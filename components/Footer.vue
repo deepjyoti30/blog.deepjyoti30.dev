46692f880f90ad060a37b81e0c89d0332c22ef23
Deepjyoti Barman <deep.barman30@gmail.com>
2021-03-04 21:44:32 +0530

    Add initial content for the Footer

---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="footer__container">
+        <div class="top--statement"></div>
+        <div class="bottom--content mt-8 flex justify-between py-6 border-t-2">
+            <div class="copyright--content">
+                © 2020-Present Deepjyoti Barman. All rights reserved.
+            </div>
+            <div class="social--content"></div>
+        </div>
+    </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,5 +2,6 @@
   <div class="xl:w-2/6 md:w-3/5 w-11/12 mr-auto ml-auto">
     <Navbar />
     <Nuxt />
+    <Footer />
   </div>
 </template>
