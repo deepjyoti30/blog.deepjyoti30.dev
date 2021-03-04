6e6557b2a4fe9c8f7d85aea9c03b39136ca36a33
Deepjyoti Barman <deep.barman30@gmail.com>
2021-03-04 22:11:49 +0530

    Change the position of the copyright text

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="footer__container">
         <div class="top--statement"></div>
-        <div class="bottom--content mt-8 flex justify-between py-6 border-t-2">
+        <div class="bottom--content mt-8 py-6 border-t-2 text-gray-500 text-center">
             <div class="copyright--content">
                 © 2020-Present Deepjyoti Barman. All rights reserved.
             </div>
