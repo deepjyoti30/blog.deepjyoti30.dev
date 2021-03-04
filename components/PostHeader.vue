97ee152bd525f0e9d72155de0cb119f94243d227
Deepjyoti Barman <deep.barman30@gmail.com>
2021-03-04 20:42:11 +0530

    Make the header extra content responsive

--- components/PostHeader.vue
+++ components/PostHeader.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="post--header__container">
         <h1 class="md:text-4xl text-3xl dm-sans font-semibold text-darkblue">{{post.title}}</h1>
-        <div class="more--details flex text-gray-500 md:mt-4 my-8 font-medium">
+        <div class="more--details md:flex text-gray-500 md:mt-4 my-8 font-medium">
             <div class="author--details inline-flex items-center">
                 <span class="author--name text-darkblue">{{post.author.name}}</span>
                 <span class="author--username ml-3">@{{post.author.username}}</span>
             </div>
-            <div class="separator h-5 w-0.5 bg-skyblue mx-2"></div>
+            <div class="separator md:h-5 md:w-0.5 h-0.5 w-10 bg-skyblue md:mx-2 my-2"></div>
             <div class="time--details inline-flex items-center">
                 <span class="date">{{getDate}}</span>
                 <span class="time ml-3">{{getTime}}</span>
