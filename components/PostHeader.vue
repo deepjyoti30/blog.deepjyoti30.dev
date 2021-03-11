76309c6218532771fd61d23001b0f16b5324018a
Deepjyoti Barman <deep.barman30@gmail.com>
2021-03-11 11:29:21 +0530

    Remove the share button from the header

--- components/PostHeader.vue
+++ components/PostHeader.vue
@@ -6,26 +6,21 @@
       {{ post.title }}
     </h1>
     <div
-      class="more--details md:flex justify-between items-center text-gray-500 dark:text-gray-400 md:mt-4 md:my-0 mt-8 mb-2 font-medium"
+      class="more--details md:flex items-center text-gray-500 dark:text-gray-400 md:mt-4 md:my-0 mt-8 mb-2 font-medium"
     >
-      <div class="details--content md:flex">
-        <div class="author--details inline-flex items-center">
-          <span class="author--name text-darkblue dark:text-darkblue-300">{{
-            post.author.name
-          }}</span>
-          <span class="author--username ml-3">@{{ post.author.username }}</span>
-        </div>
-        <div
-          class="separator md:h-5 md:w-0.5 h-0.5 w-10 bg-skyblue md:mx-2 my-2"
-        ></div>
-        <div class="time--details inline-flex items-center">
-          <span class="date mr-1.5">{{ getDate }}</span>
-          <span>&bullet;</span>
-          <span class="time ml-1.5">{{ getTime }}</span>
-        </div>
+      <div class="author--details inline-flex items-center">
+        <span class="author--name text-darkblue dark:text-darkblue-300">{{
+          post.author.name
+        }}</span>
+        <span class="author--username ml-3">@{{ post.author.username }}</span>
       </div>
-      <div class="share--content md:mt-0 mt-2">
-        <Share />
+      <div
+        class="separator md:h-5 md:w-0.5 h-0.5 w-10 bg-skyblue md:mx-2 my-2"
+      ></div>
+      <div class="time--details inline-flex items-center">
+        <span class="date mr-1.5">{{ getDate }}</span>
+        <span>&bullet;</span>
+        <span class="time ml-1.5">{{ getTime }}</span>
       </div>
     </div>
     <div class="tag--details my-2 inline-flex flex-wrap">
