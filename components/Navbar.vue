477aad2868a8f874cd045af66bc51134fd150db1
Deepjyoti Barman <deep.barman30@gmail.com>
2021-03-15 20:26:47 +0530

    Add functions to get the device theme and check if its dark

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -87,6 +87,7 @@ export default {
       }
 
       // Else we need a fallback
+      console.log('Need a fallback')
     },
   },
   mounted() {
--- mixins/theme.js
+++ mixins/theme.js
@@ -59,6 +59,21 @@ export const theme = {
              * valid.
              */
             return this.getTheme().toLowerCase() == "dark"
+        },
+        getDeviceTheme: function () {
+            /**
+             * Get the users device theme. We need to check if the device
+             * theme is dark or light by using CSS.
+             */
+            const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+            return darkThemeMq.matches ? "dark" : "light";
+        },
+        isDeviceDarkTheme: function () {
+            /**
+             * Check if the device has currently set the theme to dark
+             * or light.
+             */
+            return this.getDeviceTheme().toLowerCase() == "dark";
         }
     }
 }
