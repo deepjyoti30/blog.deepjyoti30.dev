eef0ec37950d9f9a678fddf33f5a8eb7882791d3
Deepjyoti Barman <deep.barman30@gmail.com>
2021-03-15 13:20:05 +0530

    Add the input that shows the URL that can be copied by the user

--- components/SharePopup.vue
+++ components/SharePopup.vue
@@ -4,15 +4,25 @@
       <div id="message" class="mb-2">
         <h3
           id="modal-frame-title"
-          class="modal__title md:text-2xl text-lg font-semibold text-gray-700 dark:text-gray-300 mb-2"
+          class="modal__title dm-sans mb-4 font-semibold text-xl"
         >
           {{ post.title }}
         </h3>
-        <h6 class="md:text-base text-sm text-gray-500 dark:text-gray-400">
+        <h6 class="text-base text-gray-500 dark:text-gray-400">
           {{ post.description }}
         </h6>
       </div>
-      <div class="btn-container w-full mb-2 mt-8 flex text-xl"></div>
+      <div class="copy__container">
+        <div class="copy--content flex flex-wrap mt-6">
+          <input
+            type="url"
+            class="p-2 flex-grow select-all truncate bg-transparent border rounded-l-md border focus:outline-none"
+            readonly
+            value="https://blog.deepjyoti30.dev/ytmdl-release-v2"
+          />
+          <button type="button" class="w-1/12 border rounded-r-md"></button>
+        </div>
+      </div>
     </Modal>
   </div>
 </template>
