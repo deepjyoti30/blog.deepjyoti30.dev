d41d6ed8f3cb2e290d0d144e88f2763f172fef1a
Deepjyoti Barman <deep.barman30@gmail.com>
2021-03-15 11:09:28 +0530

    Add a generice components for modals

---
+++ components/SharePopup.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="share--popup__container">
+    <Modal ref="modal" modal-id="share" heading="Share">
+      <div id="message" class="mb-2">
+        <h3
+          id="modal-frame-title"
+          class="modal__title md:text-2xl text-lg font-semibold text-gray-700 dark:text-gray-300 mb-2"
+        >
+          Share Post
+        </h3>
+        <h6 class="md:text-base text-sm text-gray-500 dark:text-gray-400">
+          Share the post
+        </h6>
+      </div>
+      <div class="btn-container w-full mb-2 mt-8 flex text-xl"></div>
+    </Modal>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    Modal: () => import(/* webpackPrefetch: true */ '~/components/Modal.vue'),
+  },
+  methods: {},
+}
+</script>
