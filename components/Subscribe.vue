e4e57f4592621a5298265066a8087bd64d43ee34
Deepjyoti Barman <deep.barman30@gmail.com>
2021-03-17 18:27:32 +0530

    Add init code for the subscribe component

---
+++ components/Subscribe.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="subscribe__container">
+    <div class="text--content">
+      <h1 class="text-4xl dm-sans">For the love of coding</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="md:my-16 my-10">
+    <Subscribe />
     <PostsContainer :posts="posts" no-heading />
   </div>
 </template>
 
 <script>
 import PostsContainer from '~/components/PostsContainer.vue'
+import Subscribe from '~/components/Subscribe.vue'
 
 export default {
-  components: { PostsContainer },
+  components: { PostsContainer, Subscribe },
   async asyncData() {
     // Fetch the posts based on the fetch URL
     const posts = await fetch(
