38b943dd3ff30a1dfee66ed2e43871e5f14fc883
Deepjyoti Barman <deep.barman30@gmail.com>
2021-03-17 19:30:46 +0530

    Try some other colors for the subscribe content

--- components/Subscribe.vue
+++ components/Subscribe.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="subscribe__container">
-    <div class="text--content bg-green-200 rounded py-6 px-4 text-center">
-      <h3 class="text-3xl dm-sans text-darkblue">For the love of coding</h3>
-      <p class="description text-gray-600 mt-4">
+    <div
+      class="text--content bg-gray-800 text-white rounded py-6 px-4 text-center"
+    >
+      <h3 class="text-3xl dm-sans font-bold">For the love of coding</h3>
+      <p class="description text-gray-400 mt-6">
         Subscribe to get the latest posts. I mostly write about Backend
         (Python/MongoDB/Postgres), Frontend (Vue/Tailwind/SCSS) and Linux.
       </p>
     </div>
+    <div class="subscribe--box"></div>
   </div>
 </template>
 
