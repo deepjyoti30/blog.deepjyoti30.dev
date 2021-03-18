9f67c100a80b923c54ff4993228d9dea4b95e4e6
Deepjyoti Barman <deep.barman30@gmail.com>
2021-03-18 21:28:52 +0530

    Change the name of the PWA

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,20 +75,15 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['~/assets/scss/app.scss', '~/assets/css/dracula.css', '~/assets/css/fonts/dm-sans.css', '~/assets/css/fonts/roboto.css', '~/assets/css/fonts/jetbrains-mono.css'],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/micromodal.client.js'
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
     '@nuxtjs/style-resources',
   ],
@@ -97,9 +92,7 @@ export default {
     scss: ['~/assets/scss/app.scss'],
   },
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     '@nuxtjs/feed'
   ],
@@ -148,14 +141,14 @@ export default {
     }
   ],
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
+
   pwa: {
     manifest: {
       lang: 'en',
+      name: "Blog | Deepjyoti Barman"
     },
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 
   // Change port
