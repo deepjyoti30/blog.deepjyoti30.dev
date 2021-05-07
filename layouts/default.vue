47e06ced9c21e49eee57c8ca80d288d20e0ea895
Deepjyoti Barman <deep.barman30@gmail.com>
2021-05-07 22:32:18 +0530

    Fix some sizing of the layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="bg-gray-50 dark:bg-darklow">
     <Navbar />
-    <div class="wrapper xl:w-2/6 md:w-3/5 w-11/12 mr-auto ml-auto">
+    <div
+      class="wrapper 2xl:w-2/6 xl:w-1/2 lg:w-3/5 md:w-4/5 w-11/12 mr-auto ml-auto"
+    >
       <Nuxt />
     </div>
     <Footer />
