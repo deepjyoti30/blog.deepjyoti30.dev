eba588ee6a3d0ad2665db77f73104a96936a712f
Deepjyoti Barman <deep.barman30@gmail.com>
2021-05-10 18:57:31 +0530

    Add init changes to add a scroll to top button

---
+++ components/ScrollTop.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="scroll--top__container absolute border p-4"></div>
+</template>
+
+<style lang="scss" scoped>
+.scroll--top__container {
+  bottom: 0;
+  right: 0;
+}
+</style>
