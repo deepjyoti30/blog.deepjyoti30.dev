40151750a27c92b4033a1ec16a19fa53e2b5d801
Deepjyoti Barman <deep.barman30@gmail.com>
2021-05-10 19:06:02 +0530

    Add an arrow to show on the scroll top button

--- components/ScrollTop.vue
+++ components/ScrollTop.vue
@@ -1,6 +1,21 @@
 <template>
-  <div class="scroll--top__container border p-4 fixed bg-white">Scroll</div>
+  <div class="scroll--top__container fixed bg-white">
+    <div class="scroll--content border rounded-2xl p-3">
+      <ArrowUpIcon size="2.2x" />
+    </div>
+  </div>
 </template>
+
+<script>
+import { ArrowUpIcon } from 'vue-feather-icons'
+
+export default {
+  name: 'ScrollTop',
+  components: {
+    ArrowUpIcon,
+  },
+}
+</script>
 
 <style lang="scss" scoped>
 .scroll--top__container {
