cac6175a8392a31bdfc11e73e8b9126cd49e80b8
Deepjyoti Barman <deep.barman30@gmail.com>
2021-05-10 13:17:16 +0530

    Add some init changes for the preview

--- components/PostPreview.vue
+++ components/PostPreview.vue
@@ -2,52 +2,63 @@
   <nuxt-link
     :to="getPostLink"
     :target="post.on_dev ? '_blank' : null"
-    class="post--preview__container py-2 mb-4 block"
+    class="post--preview__container mb-8 block hover:bg-gray-100 transition duration-100 ease-in p-2 rounded-lg"
   >
-    <div class="post--top--details flex text-gray-700 dark:text-gray-400">
-      <div class="date__container mb-2 dm-sans font-medium">
-        {{ getDate }}
-      </div>
-      <div v-if="post.on_dev" class="flex">
-        <span class="mx-2">&bullet;</span>
-        <span class="external--link__indicatior" title="External link">
-          <ExternalLinkIcon size="1.1x" />
-        </span>
-      </div>
-    </div>
-    <h1
-      class="md:text-2xl text-xl dm-sans font-bold text-darkblue dark:text-darkblue-300"
-    >
-      {{ post.title }}
-    </h1>
-    <div
-      class="extended--details mt-2 text-gray-700 dark:text-gray-400 flex items-center"
-    >
-      <span class="time inline-flex items-center">
-        <ClockIcon
-          size="0.8x"
-          class="mr-2 fill-current dark:text-white clock--icon"
-        />
-        <span class="text">{{ post.read_time.text }}</span>
-      </span>
-      <span v-if="timeStatus != null" class="inline-flex items-center">
-        <span class="mx-2">&bullet;</span>
-        <span
-          class="quick--detail font-medium inline-flex items-center"
-          :class="`text-${
-            timeStatus.toLowerCase() == 'quick' ? 'customgreen' : 'skyblue'
-          }`"
+    <div class="wrapper flex items-center">
+      <div class="preview--text__container">
+        <h1
+          class="md:text-xl text-lg dm-sans font-bold text-darkblue dark:text-darkblue-300"
         >
-          <component
-            :is="timeStatus.toLowerCase() == 'quick' ? 'ZapIcon' : 'CoffeeIcon'"
-            size="0.8x"
-            class="fill-current status--icon"
-          />
-          <span class="text ml-2">{{ timeStatus }} Read</span>
-        </span>
-      </span>
+          {{ post.title }}
+        </h1>
+        <div
+          class="post--top--details my-2 flex text-gray-600 dark:text-gray-400"
+        >
+          <div class="date__container dm-sans font-medium">
+            {{ getDate }}
+          </div>
+          <div v-if="post.on_dev" class="flex">
+            <span class="mx-2">&bullet;</span>
+            <span class="external--link__indicatior" title="External link">
+              <ExternalLinkIcon size="1.1x" />
+            </span>
+          </div>
+        </div>
+        <div
+          class="extended--details mt-2 text-gray-700 dark:text-gray-400 flex items-center hidden"
+        >
+          <span class="time inline-flex items-center">
+            <ClockIcon
+              size="0.8x"
+              class="mr-2 fill-current dark:text-white clock--icon"
+            />
+            <span class="text">{{ post.read_time.text }}</span>
+          </span>
+          <span v-if="timeStatus != null" class="inline-flex items-center">
+            <span class="mx-2">&bullet;</span>
+            <span
+              class="quick--detail font-medium inline-flex items-center"
+              :class="`text-${
+                timeStatus.toLowerCase() == 'quick' ? 'customgreen' : 'skyblue'
+              }`"
+            >
+              <component
+                :is="
+                  timeStatus.toLowerCase() == 'quick' ? 'ZapIcon' : 'CoffeeIcon'
+                "
+                size="0.8x"
+                class="fill-current status--icon"
+              />
+              <span class="text ml-2">{{ timeStatus }} Read</span>
+            </span>
+          </span>
+        </div>
+        <p class="desc text-gray-500 mt-2">{{ post.description }}</p>
+      </div>
+      <div class="preview--img__container w-1/2 p-2">
+        <img :src="getCover" alt="" class="rounded-md" />
+      </div>
     </div>
-    <p class="desc text-gray-500 mt-2">{{ post.description }}</p>
   </nuxt-link>
 </template>
 
@@ -116,6 +127,9 @@ export default {
         default:
           return null
       }
+    },
+    getCover() {
+      return this.post.cover_img
     },
   },
 }
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="bg-gray-50 dark:bg-darklow">
     <Navbar />
     <div
-      class="wrapper 2xl:w-2/6 xl:w-1/2 lg:w-3/5 md:w-4/5 w-11/12 mr-auto ml-auto"
+      class="wrapper 2xl:w-1/3 xl:w-1/2 lg:w-3/5 md:w-4/5 w-11/12 mr-auto ml-auto"
     >
       <Nuxt />
     </div>
