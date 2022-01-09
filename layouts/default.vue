c73a3bb34a5b3bd20f9a411a8cc62daf585d3598
Deepjyoti Barman <deep.barman30@gmail.com>
2022-01-09 21:03:52 +0530

    Add a navbar top component to show github repo link

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="bg-gray-50 dark:bg-darklow">
+    <NavbarTop />
     <Navbar />
     <div
-      class="wrapper 2xl:w-1/3 xl:w-1/2 lg:w-3/5 md:w-4/5 w-11/12 mr-auto ml-auto"
+      class="
+        wrapper
+        2xl:w-1/3
+        xl:w-1/2
+        lg:w-3/5
+        md:w-4/5
+        w-11/12
+        mr-auto
+        ml-auto
+      "
     >
       <Nuxt />
     </div>
