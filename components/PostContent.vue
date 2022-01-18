0b8ddb59306ee7d30f77b8f13f4385a7605a54ef
Deepjyoti Barman <deep.barman30@gmail.com>
2022-01-18 16:31:25 +0530

    Change the header title to merriweather instead of DM Sans

--- components/PostContent.vue
+++ components/PostContent.vue
@@ -1,16 +1,7 @@
 <template>
   <div class="post__container md:my-16 my-6">
     <div
-      class="
-        init--content
-        2xl:w-1/3
-        xl:w-1/2
-        lg:w-3/5
-        md:w-4/5
-        w-11/12
-        mr-auto
-        ml-auto
-      "
+      class="init--content 2xl:w-2/5 xl:w-3/5 lg:w-3/4 w-11/12 mr-auto ml-auto"
     >
       <PostHeader :post="post" />
     </div>
