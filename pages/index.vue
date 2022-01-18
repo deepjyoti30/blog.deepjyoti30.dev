d58e703b152aefc5118a33632009326abac8e018
Deepjyoti Barman <deep.barman30@gmail.com>
2022-01-18 16:09:53 +0530

    Make the index page support the layout changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    <PostsContainer class="my-10" :posts="posts" no-heading />
+    <div
+      class="
+        wrapper
+        2xl:w-1/3
+        xl:w-1/2
+        lg:w-3/5
+        md:w-4/5
+        w-11/12
+        mr-auto
+        ml-auto
+      "
+    >
+      <PostsContainer class="my-10" :posts="posts" no-heading />
+    </div>
   </div>
 </template>
 
@@ -11,9 +24,9 @@ export default {
   components: { PostsContainer },
   async asyncData() {
     // Fetch the posts based on the fetch URL
-    const posts = await fetch(
-      'https://apis.deepjyoti30.dev/blog/posts'
-    ).then((res) => res.json())
+    const posts = await fetch('https://apis.deepjyoti30.dev/blog/posts').then(
+      (res) => res.json()
+    )
 
     return {
       posts,
